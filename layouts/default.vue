a7dd17ae4cda164383e301de75f96146b3fc8c10
indrajit salui <indrajitsalui15@gmail.com>
2023-01-21 20:19:27 +0530

    Day-1 Basic template

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app>
+      <v-app-bar app><center>JSON Schema Validator</center></v-app-bar>
+
+      <v-main><Nuxt/> </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-container >
+      <v-row no-gutters>
+        <v-col cols="12" md="5" class= "ma-auto">
+          <v-card class="pa-2" outlined tile >
+            <center>Json Schema Here</center>
+          </v-card>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col cols="12" md="5"  class= "ma-auto" >
+          <v-card class="pa-2"  outlined tile> <center>Json Input Here</center>  </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
