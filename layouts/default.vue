1107879d1b712a6fc3f9fd46abc17d0a6f65bcac
Amit prajapati <prajapatiamit76@gmail.com>
2023-02-25 23:40:09 +0530

    Add omore tools

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,15 @@
 <template>
   <div>
-    <v-app>
-      <v-app-bar app color="navbar" elevation="0">
-        <v-app-bar-title class="logo">JSON Schema Validator</v-app-bar-title>
-      </v-app-bar>
-
-      <v-main><Nuxt /> </v-main>
-    </v-app>
+    <Nuxt /> 
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
 
 <style scoped>
-.logo {
-  font-size: 30px;
-  font-weight: bold;
-  color: #212121;
 
-}
-.theme--light.v-application {
-  background-color: var(--v-background-base, #eddcd9) !important;
-}
 </style>
